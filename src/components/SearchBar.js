8008f2dd18a40d7430a537cbb580f13ad3a74263
Carlos <17171493@itculiacan.edu.mx>
2025-07-24 03:37:54 -0700

    Agrego de componente barra de busqueda

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,12 +6,12 @@
         const [searchWord, setSearchWord] = useState("");
         
         return(
-        <div className="w-full max-w-sm min-w-[200px]">
+        <div className="min-w-xs">
             <div className="relative">
             <input
                 className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
                 placeholder="Buscar por nombre"
-                onChange={ (e) => {console.log(e.target.value);setSearchWord(e.target.value) }}
+                onChange={ (e) => {setSearchWord(e.target.value) }}
             />
             <button onClick={ () => buttonAction(searchWord) }
                 className="absolute top-1 right-1 flex items-center rounded bg-slate-800 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
